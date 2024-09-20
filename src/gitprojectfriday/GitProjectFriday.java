2cac9fceb1b17993ef2557b8d9d89b4472eaf61a
Nagma <99428751+NagmaKhattar@users.noreply.github.com>
2024-09-20 08:51:04 -0400

    Update GitProjectFriday.java

--- src/gitprojectfriday/GitProjectFriday.java
+++ src/gitprojectfriday/GitProjectFriday.java
@@ -18,6 +18,7 @@ public class GitProjectFriday {
      */
     //First change
     //changed on remote
+    //showing fetch
     public static void main(String[] args) {
         // TODO code application logic here
     }
